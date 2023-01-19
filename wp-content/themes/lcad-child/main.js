e1f92f7c8e58a6a6c16caaf0695acb877361c7b5
Aaron Singleterry <aaronsingleterry10@gmail.com>
2023-01-19 10:19:23 -0600

    finished up navbar - AS

--- wp-content/themes/lcad-child/main.js
+++ wp-content/themes/lcad-child/main.js
@@ -44,7 +44,7 @@
 // // ------------------------------------------
 // // End Scroll Trigger
 
-// Nav bar
+// Navbar
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('nav').addClass('transparent');
@@ -52,14 +52,72 @@ $(window).scroll(function() {
         $('nav').removeClass('transparent');
     }
 });
+// Navbar menu button
+function myFunction(x) {
+    let menu = document.getElementById('menu-button');
+    x.firstElementChild.classList.toggle("change");
+    if (!x.classList.contains('collapsed')) {
+        menu.innerText = 'CLOSE';
+    } else {
+        menu.innerText = 'MENU';
+    }
+}
 
 // Function to toggle the plus menu into minus 
 function myParentElem(y) {
-    if (y.getAttribute('aria-expanded') === 'true') {
-        y.firstElementChild.classList.remove("dashicons-plus-alt2");
-        y.firstElementChild.classList.add("dashicons-minus")
+    let classes = y.firstElementChild.classList
+    let academics = document.getElementById('academics').classList;
+    let admissions = document.getElementById('admissions').classList;
+    let alumni = document.getElementById('alumni').classList;
+    if (y.getAttribute('aria-expanded') === 'true' && classes.contains('academics-span')) {
+        classes.remove("dashicons-plus-alt2");
+        classes.add("dashicons-minus");
+        admissions.remove("dashicons-minus");
+        admissions.add("dashicons-plus-alt2");
+        alumni.remove("dashicons-minus");
+        alumni.add("dashicons-plus-alt2");
+    } else if (y.getAttribute('aria-expanded') === 'true' && classes.contains('admissions-span')) {
+        classes.remove("dashicons-plus-alt2");
+        classes.add("dashicons-minus");
+        academics.remove("dashicons-minus");
+        academics.add("dashicons-plus-alt2");
+        alumni.remove("dashicons-minus");
+        alumni.add("dashicons-plus-alt2");
+    } else if (y.getAttribute('aria-expanded') === 'true' && classes.contains('alumni-span')) {
+        classes.remove("dashicons-plus-alt2");
+        classes.add("dashicons-minus");
+        academics.remove("dashicons-minus");
+        academics.add("dashicons-plus-alt2");
+        admissions.remove("dashicons-minus");
+        admissions.add("dashicons-plus-alt2");
     } else {
-        y.firstElementChild.classList.remove("dashicons-minus")
-        y.firstElementChild.classList.add("dashicons-plus-alt2")
+        classes.remove("dashicons-minus");
+        classes.add("dashicons-plus-alt2");
     }
 }
+
+// Function to split the sub menu items list that are > 4
+function splitUl(list) {
+    let newList = '';
+    list.each((index, element) => {
+        if (index === 0) {
+            newList += `<ul class="menu-sub-items"><li class="page_item"> ${element.innerHTML} </li>`;
+        } else if ([index + 1] % 4 === 0 && index !== list.length - 1) {
+            newList += `<li class="page_item"> ${element.innerHTML} </li></ul><ul class="menu-sub-items">`;
+        } else if (index === list.length - 1) {
+            newList += `<li class="page_item"> ${element.innerHTML} </li></ul>`;
+        }  else {
+            newList += `<li class="page_item"> ${element.innerHTML} </li>`;
+        }
+    });
+    return newList;
+}
+let academics = $('.academics-menu li');
+let admissions = $('.admissions-menu li');
+let alumniStudentLife = $('.alumni-student-life-menu li');
+let academicsMenu = splitUl(academics);
+let admissionsMenu = splitUl(admissions);
+let alumniStudentLifeMenu = splitUl(alumniStudentLife);
+$('.academics-menu').html(academicsMenu);
+$('.admissions-menu').html(admissionsMenu);
+$('.alumni-student-life-menu').html(alumniStudentLifeMenu);
