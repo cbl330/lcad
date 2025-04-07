82473031b02c6999e6bb0068ee143689fc5194c0
Chris Lugenbeal <cblugenbeal@gmail.com>
2025-04-06 22:59:10 -0400

    Remove all files except wp-content from repo

--- wp-content/plugins/genesis-blocks/dist/blocks.build.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-element'), 'version' => 'bec47b16e658e04b0ba9');
--- wp-content/plugins/simple-page-ordering/dist/js/simple-page-ordering.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery', 'jquery-ui-sortable'), 'version' => 'abf1d95e6c2b031b104a');
